ce9bf70b970b97a0190fb2641e97842105a120b1
Mario <ruczkowski.m@gmail.com>
2024-02-20 16:01:44 +0000

    change footer tekst

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
     <div>
       <p>
         <a href="mailto:taniestrony@protonmail.com" className="footer">
-          Tanie strony internetowe - wyslij e-mail z zapytaniem!
+          Umieść swoją reklamę tutaj - wyślij e-mail z zapytaniem!
         </a>
       </p>
     </div>
